f1897dd0cf71cd6c9beaa390c69d9c0fbaba475f
akpsaheer <akpsaheer@gmail.com>
2026-05-13 15:45:30 +0530

    Fix unresolved reference 'DialogProperties' by adding missing import

--- app/src/main/java/com/example/qamoos/MainActivity.kt
+++ app/src/main/java/com/example/qamoos/MainActivity.kt
@@ -52,6 +52,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
